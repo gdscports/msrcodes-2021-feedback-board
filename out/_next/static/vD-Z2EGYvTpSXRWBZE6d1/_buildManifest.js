b7d206264d5327a3e225fa59cf485b54690339bb
Mikael Rozee <mikaelsrozee@gmail.com>
2021-08-25 16:01:12 +0100

    refactor: remove out directory from gh

--- out/_next/static/vD-Z2EGYvTpSXRWBZE6d1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/158-d753aae370d772435292.js","static/chunks/pages/index-ebfb6af5e69a94dde5d6.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
